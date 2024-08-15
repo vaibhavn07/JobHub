6343b37b020f563115a3f78636e981ea5e63a578
vaibhavn07 <121496917+vaibhavn07@users.noreply.github.com>
2024-08-15 22:32:12 +0530

    updated

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const URI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1"
 
 mongoose.connect(process.env.URI)
 .then(()=>{console.log("connected")})
